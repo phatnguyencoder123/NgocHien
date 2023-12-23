0c116fb25a6db5e4443913b59bda3606923a2537
phatnguyencoder123 <nguyenhuynhtanphat11a7@gmail.com>
2023-12-24 02:08:25 +0700

    upload source code

--- scriipt.js
+++ scriipt.js
@@ -1,7 +1,7 @@
 let container = document.querySelector('.container');
 let count = 30;
 for(var i = 0; i<200; i++){
-    let leftSnow = Math.floor(Math.random() *3* container.clientWidth);
+    let leftSnow = Math.floor(Math.random() *1* container.clientWidth);
     let topSnow = Math.floor(Math.random() *-1* container.clientHeight);
     let widthSnow = Math.floor(Math.random() * 50);
     let timeSnow = Math.floor((Math.random() * 5) + 5);
